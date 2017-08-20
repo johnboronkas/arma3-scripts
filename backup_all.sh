b6bfa26e6ef9bf17a35e101d7652a6b37ca53f70
John Boronkas <johnboronkas@gmail.com>
2017-08-20 14:54:34 -0400

    Fixed arma3profile location.

--- backup_all.sh
+++ backup_all.sh
@@ -4,7 +4,6 @@ _arma3="C:\\Program Files (x86)\\Steam\\steamapps\\common\\Arma 3\\"
 cp "$_arma3""jdb_hc.bat" ".\\server\\"
 cp "$_arma3""jdb_mp.cfg" ".\\server\\"
 cp "$_arma3""jdb_mp_start.bat" ".\\server\\"
-cp "$_arma3""Users\\jdb_mp_server\\jdb_mp_server.Arma3Profile" ".\\server\\"
 
 _profiles="C:\\Users\\jd\\Documents\\Arma 3 - Other Profiles\\"
 cp "$_profiles""jdb_mp_server\\jdb_mp_server.Arma3Profile" ".\\server\\"
--- restore_all.sh
+++ restore_all.sh
@@ -4,7 +4,6 @@ _arma3="C:\\Program Files (x86)\\Steam\\steamapps\\common\\Arma 3\\"
 cp .\\server\\jdb_hc.bat "$_arma3"
 cp .\\server\\jdb_mp.cfg "$_arma3"
 cp .\\server\\jdb_mp_start.bat "$_arma3"
-cp .\\server\\jdb_mp_server.Arma3Profile "$_arma3""\\Users\\jdb_mp_server\\"
 
 _profiles="C:\\Users\\jd\\Documents\\Arma 3 - Other Profiles\\"
 cp ".\\server\\""jdb_mp_server.Arma3Profile" "$_profiles""jdb_mp_server\\"
